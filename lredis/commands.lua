6809007d67c5990a57c637f79ae5c1c6ef600b35
daurnimator <quae@daurnimator.com>
2015-09-22 01:44:58 +1000

    lredis/commands: Create reuseable return handler

--- lredis/commands.lua
+++ lredis/commands.lua
@@ -1,15 +1,19 @@
 local methods = {}
 
-function methods:ping()
-	local resp = self:call("PING")
+local function handle_ok_or_err(resp)
 	local is_table = type(resp) == "table"
 	if is_table and resp.ok then
 		return resp.ok
 	elseif is_table and resp.err then
 		error(resp.err, 2)
 	else
-		error("unexpected PING response")
+		error("unexpected response format")
 	end
+end
+
+function methods:ping()
+	local resp = self:call("PING")
+	return handle_ok_or_err(resp)
 end
 
 function methods:subscribe(...)
